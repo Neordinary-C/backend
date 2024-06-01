ca7517dcf47dbdd27d9a4b519f659def44538905
YH <imrubi5555@gmail.com>
2024-06-02 07:30:29 +0900

    no message

--- src/user/dto/update-status.dto.ts
+++ src/user/dto/update-status.dto.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { IsString, IsIn } from 'class-validator';
 
 export class UpdateStatusDto {
@@ -8,19 +7,4 @@ export class UpdateStatusDto {
   @IsString()
   @IsIn(['ongoing', 'success', 'failed'])
   readonly status: string;
-=======
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
-
-import { IsString, IsIn, IsNumber } from 'class-validator';
-
-
-export class UpdateStatusDto {
-    @IsString()
-    readonly user_id: string;
-
-    @IsString()
-    @IsIn(['ongoing', 'success', 'failed'])
-    readonly status: string;
->>>>>>> 91c0a2c14231f152c58561acdf07c4a4908b72d7
 }
