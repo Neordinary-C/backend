8a04f294b1274aae1022f61146814d331a07a1e3
YH <imrubi5555@gmail.com>
2024-06-01 15:26:35 +0900

    [초기화] NestJS 및 Swagger 초기 세팅

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './all-exceptions';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  /* 기본 루트: /api */
+  app.setGlobalPrefix('api');
+
+  /* Swagger */
+  const config = new DocumentBuilder()
+    .setTitle('UMC 6th Hackathon Team C - Swagger API 문서')
+    .setDescription('Swagger 문서 페이지입니다.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
